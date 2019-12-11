988aeb73d7764a4e2275fec3c194a29f5fab109d
Soohwan <soohwan.ok@navercorp.com>
2019-12-11 23:28:36 +0900

    RetentionPolicy 변경

--- app/src/main/java/com/charlezz/annotationprocessorstudy/MainActivity.java
+++ app/src/main/java/com/charlezz/annotationprocessorstudy/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Charles.intentForSecondActivity(this);
+        startActivity(Charles.intentForSecondActivity(this));
     }
 }
